fa533e3ee483e803216b504130399d17ca5a44ce
Nico Auerswald <auersw.nico@gmail.com>
2024-11-16 14:28:54 +0100

    feat: break down scenes

--- elevator/scripts/crank.gd
+++ elevator/scripts/crank.gd
@@ -1,5 +1,13 @@
 extends StaticBody3D
 
+@export var mesh : MeshInstance3D
+@export var highlight_material : Material
 
 func interact():
 	Elevator.add_charge(1.0)
+
+func highlight():
+	mesh.material_override.next_pass = highlight_material
+	
+func clear_highlight():
+	mesh.material_override.next_pass = null
