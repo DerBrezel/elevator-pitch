fc82e6900f1d29b8c7cc866b53de58fecb83e185
Nico Auerswald <auersw.nico@gmail.com>
2024-11-16 15:03:31 +0100

    feat: add highlight logic

--- core/scripts/interactor.gd
+++ core/scripts/interactor.gd
@@ -2,22 +2,28 @@ extends RayCast3D
 
 @onready var mesh: MeshInstance3D = $"../../Button/MeshInstance3D"
 
+var current_target : Node
 
 func _process(delta):
 	# Continuously check for an object in the center of the screen
 	if is_colliding():
 		var target = get_collider()
 		
-		if target:
-			# Example: Highlight or interact with the target
+		if target:			
+			if current_target == null:
+				current_target = target
+				if target.has_method("highlight"):
+					target.highlight()
 			print("Looking at:", target.name)
 			# Check if the target has a method or script to interact with
 			if Input.is_action_pressed("interact"):
-				change_mesh_color(Color(1,0,0))
 				interact_with_target(target)
-			else:
-				change_mesh_color(Color(1,1,1))
+
 	else:
+		if current_target != null:
+			if current_target.has_method("clear_highlight"):
+				current_target.clear_highlight()
+				current_target = null
 		print("No object in the center.")
 
 func interact_with_target(target):
@@ -25,14 +31,3 @@ func interact_with_target(target):
 		return
 	# Call the interact method on the target (if defined)
 	target.interact()
-
-# Call this function with the desired color when you want to change it
-func change_mesh_color(new_color: Color) -> void:
-	# Ensure the mesh has a material you can modify
-	var material := mesh.get_surface_override_material(0)
-	if material == null:
-		material = StandardMaterial3D.new()
-		mesh.set_surface_override_material(0, material)
-	
-	# Change the albedo color
-	material.albedo_color = new_color
--- elevator/scripts/crank.gd
+++ elevator/scripts/crank.gd
@@ -1,13 +1,14 @@
 extends StaticBody3D
 
 @export var mesh : MeshInstance3D
+@export var default_material : Material
 @export var highlight_material : Material
 
 func interact():
 	Elevator.add_charge(1.0)
 
 func highlight():
-	mesh.material_override.next_pass = highlight_material
+	mesh.set_surface_override_material(0, highlight_material)
 	
 func clear_highlight():
-	mesh.material_override.next_pass = null
+	mesh.set_surface_override_material(0,default_material)
