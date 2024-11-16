2e4d2d55c24dc46624eba6f0cb3e7a22e30023aa
Nico Auerswald <auersw.nico@gmail.com>
2024-11-16 15:55:48 +0100

    refactor: use seperate mesh for highlight

--- elevator/scripts/crank.gd
+++ elevator/scripts/crank.gd
@@ -1,14 +1,15 @@
 extends StaticBody3D
 
-@export var mesh : MeshInstance3D
-@export var default_material : Material
-@export var highlight_material : Material
+@export var mesh_highlight : MeshInstance3D
+
+func _ready() -> void:
+	mesh_highlight.visible = false
 
 func interact():
 	Elevator.add_charge(1.0)
 
 func highlight():
-	mesh.set_surface_override_material(0, highlight_material)
+	mesh_highlight.visible = true
 	
 func clear_highlight():
-	mesh.set_surface_override_material(0,default_material)
+	mesh_highlight.visible = false
