57f1bcf3317ab4d86a96188d9d21172a26aa14fe
Ruben <schaefer-ruben@web.de>
2024-11-17 13:54:56 +0100

    feat: improve

--- character/billy.gd
+++ character/billy.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+var hasArrived := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,7 +11,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Elevator.machine.current_state.name == "Enter":
+	if Elevator.machine.current_state.name == "Enter" and not hasArrived:
+		hasArrived = true
 		animation_player.play("enter")
-		await animation_player.animation_finished
-		Elevator.machine.change_state("Crank")
+		#await animation_player.animation_finished
+		#Elevator.machine.change_state("Crank")
